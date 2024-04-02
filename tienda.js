4b19b72b6aef927e775f75a4df9365af1fa90826
MCcampos <mc.campos.2020@alumnos.urjc.es>
2024-04-02 14:04:06 +0200

    Ya sale TRUE al poner el usuario y contraseña

--- tienda.js
+++ tienda.js
@@ -6,13 +6,21 @@ const PUERTO = 9090;
 const FORMULARIO = fs.readFileSync('login.html', 'utf-8');
 const RESPUESTA = fs.readFileSync('gorrito1.html', 'utf-8'); //!VER 
 
+//----------json
+DATAJSON =  fs.readFileSync('tienda.json', 'utf-8')
+DATAJSON = JSON.parse(DATAJSON)
+console.log(DATAJSON)
 cargarTienda("tienda.json")
 
+
+//-------
 const server = http.createServer((req, res) => {
     const url = req.url === '/' ? '/tienda.html' : req.url;
     const filePath = path.join(__dirname, url);
     const extension = path.extname(filePath);
     
+    //console.log("  Ruta: " + url);
+
     let contentType = 'text/html';
 
     switch (extension) {
@@ -40,13 +48,11 @@ const server = http.createServer((req, res) => {
         break;
     }
 
+    
+
     if (req.method === 'POST' && req.url === '/procesar') {
 
-          //-- Si hay datos en el cuerpo, se imprimen
         req.on('data', (cuerpo) => {       
-          //-- Los datos del cuerpo son caracteres
-          //TODO: Ahora que me lee usuario y contraseña, ¿que hago?
-          //TODO: veo si es igual al JSON??
           req.setEncoding('utf8');
           console.log(` ${cuerpo}`);
         });        
@@ -59,6 +65,22 @@ const server = http.createServer((req, res) => {
             res.write(RESPUESTA);
             res.end();
         });
+
+        
+        if (url == '/procesar'){
+            console.log('----Login----')
+            req.on('data', (content)=> {
+                content = (content.toString()).split("&")
+                content =  convert2Dic(content,"=")
+                if(content['userName'] != ""){
+                    console.log(content['userName'])
+                    //todo: ver si el usuario existe, mediante una función.
+                    check = checkUser(content['userName'] , content['password'] ,DATAJSON)
+                    console.log(check)
+                }
+            });
+        }
+
     } else {
         // Manejar las solicitudes GET para otros recursos
         fs.readFile(filePath, (err, content) => {
@@ -79,9 +101,7 @@ const server = http.createServer((req, res) => {
             }
         });
     }
-    
    
-    //------------
 });
 
 
@@ -95,17 +115,51 @@ server.listen(PUERTO, () => {
 //------------Lectura JSON
 //JSON.stringify(variable)inversa de variable a json
 function cargarTienda(nombreArchivo) {
+    let content = RESPUESTA;
     try {
         const tienda_json = fs.readFileSync(nombreArchivo);
         const tienda = JSON.parse(tienda_json);
         console.log("Productos en la tienda: " + tienda.productos.length);
-        tienda.productos.forEach((productos, index)=>{
-            console.log("Producto " + (index + 1) + ": " + productos.nombre);
-          });
+        //tienda.productos.forEach((productos, index)=>{
+        //    console.log("Producto " + (index + 1) + ": " + productos.nombre);
+        //});
+          let nombre = tienda.productos[0].nombre;
+          console.log(nombre)
+          content = content.replace("NOMBRE", nombre);
         return tienda;
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
         return null;
     }
 }
-//----------------------------------------
+
+
+
+
+//---------------------FUNCIONES--------------------------
+
+function convert2Dic(params , split){
+
+    const dict = {};
+    for (let i = 0; i < params.length; i++){
+      param = params[i].split(split)
+      dict[param[0]] = param[1];
+    }
+    return dict
+  }
+  
+  //metemos en la función el usuario, contraseña, y el Json donte tenemos los usuarios
+  function checkUser(usuario,password,DATAJSON){
+    found = false
+    for (let i = 0; i <  DATAJSON.nombres.length; i++){
+        console.log('usuario que pongo yo',usuario)
+        console.log('Contraseña',password)
+
+      if(DATAJSON.nombres[i].usuario == usuario && DATAJSON.nombres[i].password == password ){
+        
+        found = true;
+        break;
+      }
+    }
+    return [found]
+  }
